a3feb4717d0a2d4a2ed51f5b9800226d6487b4a6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-06 22:58:53 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -21,6 +21,10 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
   end
 end
