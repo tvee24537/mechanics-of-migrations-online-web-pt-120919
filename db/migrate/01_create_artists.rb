26a23c5498cde01071c91a0df7d1d679555dbce2
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-06 22:58:20 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,8 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite")
-  
+
   sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
   id INTEGER PRIMARY KEY,
