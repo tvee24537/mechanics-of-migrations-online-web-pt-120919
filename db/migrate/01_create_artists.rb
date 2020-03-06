f5c037dad87030ff8b9028420535837e61698052
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-06 22:56:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite")
+  
+  
   def up
   end
  
